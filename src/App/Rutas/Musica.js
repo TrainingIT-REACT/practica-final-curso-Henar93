076067fe0393d9761c7b780b6c90bfd4fe79f78f
Henar Lorenzo Serrano <henar.lorenzo@ALTIACOR.ES>
2020-02-19 18:12:23 +0100

    5.1

--- src/App/Rutas/Musica.js
+++ src/App/Rutas/Musica.js
@@ -1,13 +1,56 @@
 import React from 'react';
 
-// Este componente utiliza el parámetro para mostrarlo en la interfaz
-const Admin = () => {
-  return <div>
-    <h2>Código de acceso</h2>
-    <p>
-      El código de acceso secreto para hoy es: <b>React16</b>
-    </p>
-  </div>;
+import { connect } from "react-redux";
+
+import { getSongsByAlbum } from '../storeMusic/StoreMusic';
+import { getAlbumById } from '../storeMusic/StoreMusic';
+import Songs from "../StoreMusic/Songs";
+import Loader from '../Menu/Loader';
+
+
+class Album extends React.Component{
+    componentDidMount() {
+        this.props.getAlbumById(this.props.match.params.id);
+        this.props.getSongsByAlbum(this.props.match.params.id);
+    }
+
+    render(){
+         let songs=this.props.albums.songs;
+        let album=this.props.albums.album;
+      
+
+        if (this.props.albums.isLoading) {
+            return <Loader />
+        } else if (this.props.albums.error) {
+            return <p>Error al obtener los datos</p>
+        } else if (this.props.albums.songs && this.props.albums.album) {
+            return <div className="album">
+                <div className="header">
+                    <div className="image">
+                        <img src={album.cover} alt="Imagen del álbum" title="Imagen del álbum" />
+                    </div>
+                    <div className="data">
+                        <h2 className="title">{album.name}</h2>
+                        <h3 className="author">De <i>{album.artist}</i></h3>
+                         <div className="songs">Nº canciones: {songs.lenkgth}</div> 
+                    </div>
+                </div>
+                 <Songs data={songs} album={album.name}/>
+            </div>
+        } else{
+            return <Loader />
+        }
+    }
 }
 
-export default Admin;
+const mapStateToProps = (state) => ({ ...state });
+
+const mapDispatchToProps = (dispatch) => ({
+    getAlbumById: (id) => dispatch(getAlbumById(id)),
+    getSongsByAlbum: (id) => dispatch(getSongsByAlbum(id))
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Album);
